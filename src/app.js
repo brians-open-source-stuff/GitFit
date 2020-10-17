8272553874b509aa38077344796778e7eb4b0b29
Brian Emilius <brian@brianemilius.com>
2020-10-17 16:15:52 +0200

    add basic styled components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,51 @@
 import React, { Fragment } from "react";
+import { Router, Link, Redirect } from "@reach/router";
 import { render } from "react-dom";
+import Sets from "./Sets";
+import "./style.scss";
+
+
+function Recent() {
+	return (
+		<h1>Recent</h1>
+	);
+}
+function Timer() {
+	return (
+		<h1>Timer</h1>
+	);
+}
 
 function App() {
 	return (
 		<Fragment>
-			<h1>GitFit</h1>
+			<header>
+				<img src="https://via.placeholder.com/100/100" class="profileImage" /> @BrianEmilius
+			</header>
+
+			<main>
+				<Router>
+					<Redirect from="/" to="sets" />
+					<Sets path="sets" />
+					<Sets path="sets/:id" />
+					<Recent path="recent" />
+					<Timer path="timer" />
+				</Router>
+			</main>
+
+			<nav class="drawer">
+				<ul class="drawer__iconList">
+					<li>
+						<Link to="/sets">Sets</Link>
+					</li>
+					<li>
+						<Link to="recent">Recent</Link>
+					</li>
+					<li>
+						<Link to="timer">Timer</Link>
+					</li>
+				</ul>
+			</nav>
 		</Fragment>
 	);
 }
