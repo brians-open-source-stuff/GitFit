f01f2002714fd7415efb2974acc65b17d56b0db8
Brian Emilius <brian@brianemilius.com>
2020-10-17 14:29:14 +0200

    add basic structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { render } from "react-dom";
+
+function App() {
+	return (
+		<Fragment>
+			<h1>GitFit</h1>
+		</Fragment>
+	);
+}
+
+render(<App />, document.getElementById("root"));
